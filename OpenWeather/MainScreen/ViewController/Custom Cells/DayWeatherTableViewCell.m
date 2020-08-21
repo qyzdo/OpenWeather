d21556629c15e1eb3d6b6a0d0bb7d4328379a8df
Oskar Figiel <qyzdo@outlook.com>
2020-08-21 02:11:08 +0200

    Remove unused code

--- OpenWeather/MainScreen/ViewController/Custom Cells/DayWeatherTableViewCell.m
+++ OpenWeather/MainScreen/ViewController/Custom Cells/DayWeatherTableViewCell.m
@@ -9,11 +9,6 @@
 
 @implementation DayWeatherTableViewCell
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
-}
-
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
 
